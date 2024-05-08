bf6ef00d7db4eac716f6bd64ea8b51006ba100a8
NicolasCasamen <sebastian.casamen@udla.edu.ec>
2024-05-08 11:45:47 -0500

    Database

---
+++ Data/Models/Promo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_Burger.NicolasCasamen.Data.Models;
+
+public partial class Promo
+{
+    public int Id { get; set; }
+
+    public int PromoId { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public DateTime FechaPromo { get; set; }
+
+    public int? BurgerId { get; set; }
+
+    public virtual Burger? Burger { get; set; }
+}
